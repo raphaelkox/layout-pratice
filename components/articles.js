af65f7aea18b0b2b5768455d312feabe4458ed81
RaphaelKox <raphaelkox@users.noreply.github.com>
2021-01-20 17:33:17 -0300

    articles from db

--- components/articles.js
+++ components/articles.js
@@ -1,22 +1,20 @@
-export default function Articles(){
-    const contentCount = 5;
-    let contentList = Array.from(Array(contentCount).keys());    
-
-    let imageIds = ['00', '01', '02', '03', '04'];
+export default function Articles(props){    
     let imagesPath = "images/";
 
     return(
         <>
-            {contentList.map(x => {
+            {props.articles.map(x => {
                 return(
                 <div className="p-1 w-full md:w-1/2 md:first:w-full h-96 bg-red-700" key={"contentSlot" + x} >
                     <div className="p-1 w-full h-full flex flex-col justify-around rounded-md bg-red-500" >
-                        <div className="w-full text-2xl">ARTICLE TITLE</div>
-                        <div className="w-full text-xl">ARTICLE SUBTITLE</div>
+                        <div className="w-full text-2xl">{x.title}</div>
+                        <div className="w-full text-xl">{x.subtitle}</div>
                         <div className="overflow-hidden w-full h-48">
-                            <img className="object-cover w-full h-full transition-transform transform xl:hover:scale-110" src={imagesPath + imageIds[x] + ".jpg"} loading="lazy" />
+                            <img className="object-cover w-full h-full transition-transform transform xl:hover:scale-110" src={imagesPath + imageIds[x.imageId] + ".jpg"} loading="lazy" />
                         </div>
-                        <div className="w-full text-sm">Article Metadata: Author, Date, Tags</div>
+                        <div className="w-full text-sm">Tags: {x.tags}</div>
+                        <div className="w-full text-sm">{moment(x.date).fromNow()}</div>
+                        <div className="w-full text-sm">por Raphael Kox</div>                        
                     </div>
                 </div>
                 )
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
 import Head from 'next/head'
-import Layout from '../components/layout'
 import LayoutV2 from '../components/layoutv2'
 import Articles from '../components/articles'
+import { connectToDatabase } from '../util/mongodb'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <LayoutV2>
       <Head>
         <title>Home</title>
       </Head>
-      <Articles />
+      <Articles articles={articles}/>
     </LayoutV2>
   )
+}
+
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase();
+
+  const articles = await db
+      .collection("Next_integration_mockdata")
+      .find({})
+      .limit(10)
+      .toArray();
+  
+  return {
+      props: {
+          movies: JSON.parse(JSON.stringify(articles)),
+      },
+  };
 }
